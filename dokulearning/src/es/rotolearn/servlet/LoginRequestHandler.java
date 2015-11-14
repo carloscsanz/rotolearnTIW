d55592dfc37d9bce8baa1a7e4f7d23112b5c2c15
Carlos Contreras Sanz <carlosdemota@gmail.com>
2015-11-14 17:08:43 +0100

    DATASOURCE login y registro dokulearning

--- dokulearning/src/es/rotolearn/servlet/LoginRequestHandler.java
+++ dokulearning/src/es/rotolearn/servlet/LoginRequestHandler.java
@@ -1,7 +1,14 @@
 package es.rotolearn.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.Statement;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -10,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
 
 import es.rotolearn.entities.Usuario;
 import es.rotolearn.javaBean.RegistroBean;
@@ -21,14 +29,108 @@ public class LoginRequestHandler implements RequestHandler {
 		HttpSession session;
 		System.out.println("Handler login received the request");
 		//HAY QUE AÑADIR/MODIFICAR PARA METERLE LA BBDD
-		String ruta;
+		String ruta = null;
 		String nick = request.getParameter("Nickname");
 		String pass = String.valueOf(request.getParameter("Password").hashCode());
 		System.out.println("Nick introducido: "+nick);
 		System.out.println("Pass introducida: "+pass);
 		
+		
+		//Creamos el usuario a buscar en la BBDD
+				Usuario newUsuario = new Usuario();	
+				newUsuario.setNickname(nick);
+				
+				/*Insercion a BBDD con DataSource*/
+				System.out.println("Vamos a probar a hacer la insercion por DATASOURCE");
+				InitialContext miInitialContext;
+				DataSource miDS;
+				try{
+					miInitialContext = new InitialContext();
 
-			/*HAGO LA CONSULTA A LA BBDD*/
+					miDS = (DataSource) miInitialContext.lookup("RotolearnJNDI");
+
+					Connection conexion = miDS.getConnection();
+
+					Statement myStatement = conexion.createStatement();
+					System.out.println("Antes de la query");
+					ResultSet rs = myStatement.executeQuery("SELECT * FROM USUARIO WHERE Nickname='"+nick+"'");
+					System.out.println("Despues de la query");
+					
+				if(rs == null){ // No existe el usuario
+					System.out.println("rs = = NULL");
+					request.setAttribute("error", "true");
+					ruta = "login.jsp";
+				}else{//existe el nick, comparar contraseñas
+						rs.next();// PONER ESTO ANTES DE HACER NADA SOBRE UNA RESULSET
+						System.out.println("ENTRO A COMPARAR CONTRASEÑAS");
+						System.out.println("rs.getstrin " + rs.getString(6) + " pass "+pass);
+						if(rs.getString("Pass").equals(pass)){//las contraseñas coinciden login valido
+							RegistroBean regbean = new RegistroBean();
+							regbean.setNickName(rs.getString("Nickname"));
+							regbean.setApellido1(rs.getString("Apellido1"));
+							regbean.setApellido2(rs.getString("Apellido2"));
+							regbean.setTipo(rs.getString("Tipo"));
+							regbean.setEmail(rs.getString("Email"));
+							regbean.setDescripcion(rs.getString("Descripcion"));
+							regbean.setIntereses("Esto hay que cambiarlo");//por si no te has dado cuenta, esrto hay que cambiarlo.
+							regbean.setTelefono(rs.getString("Telefono"));
+							regbean.setNacimiento(rs.getString("Fecha_nac"));
+						    regbean.setDireccion(rs.getString("Direccion"));
+						    regbean.setNombre(rs.getString("Nombre"));	
+							//System.out.println("Pass correcta, puede entrar");
+							//System.out.println("Paso comparaciond de pass ");
+							session = ((HttpServletRequest) request).getSession();
+							session.setAttribute("logueado", "true");
+							session.setAttribute("usuario",nick);
+							session.setAttribute("perfil",regbean);						
+							ruta = "index.jsp";
+							myStatement.close();
+							conexion.close();
+							
+						}else{ 			//FALLO EN LA PASS			
+								request.setAttribute("error", "pass");
+								ruta = "login.jsp";
+							
+							 }				
+			
+					}	//fin else
+				}//fin try
+				catch (NamingException e) {
+					// TODO Bloque catch generado automaticamente
+					e.printStackTrace();
+					request.setAttribute("error", "true");
+					ruta = "login.jsp";
+
+				} catch (SQLWarning sqlWarning) {
+					while (sqlWarning != null) {
+						System.out.println("Error: " + sqlWarning.getErrorCode());
+						System.out.println("Descripcion: " + sqlWarning.getMessage());
+						System.out.println("SQLstate: " + sqlWarning.getSQLState());
+						sqlWarning = sqlWarning.getNextWarning();
+						request.setAttribute("error", "true");
+						ruta = "login.jsp";
+					}
+				} catch (SQLException sqlException) {
+					while (sqlException != null) {
+						System.out.println("Error: " + sqlException.getErrorCode());
+						System.out.println("Descripcion: " + sqlException.getMessage());
+						System.out.println("SQLstate: " + sqlException.getSQLState());
+						sqlException = sqlException.getNextException();
+						request.setAttribute("error", "true");
+						ruta = "login.jsp";
+					}
+				}
+				
+		
+			
+		
+		
+		
+		
+		
+		
+		/*
+			HAGO LA CONSULTA A LA BBDD
 			// 1 Create the factory of Entity Manager
 			EntityManagerFactory factory = Persistence.createEntityManagerFactory("dokulearning");//ESTO ES CLAVE
 
@@ -98,8 +200,9 @@ public class LoginRequestHandler implements RequestHandler {
 				System.out.println("C L O S E ");
 			}
 		
+	*/
+		
 	
-		return ruta;
+return ruta;
 	}
-
 }
--- dokulearning/src/es/rotolearn/servlet/RegistroRequestHandler.java
+++ dokulearning/src/es/rotolearn/servlet/RegistroRequestHandler.java
@@ -2,7 +2,14 @@
 package es.rotolearn.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.Statement;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -11,6 +18,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import es.rotolearn.entities.Usuario;
 import es.rotolearn.javaBean.RegistroBean;
@@ -22,7 +30,93 @@ public class RegistroRequestHandler implements RequestHandler {
 		String ruta = "login.jsp";
 		System.out.println("Creamos el usuario");
 		
-		/*HAGO  LA CONSULTA A LA BBDD*/
+		//Creamos el usuario a buscar en la BBDD
+		Usuario newUsuario = new Usuario();
+		String nick = request.getParameter("nick");		
+		newUsuario.setNickname(nick);
+		
+		/*Insercion a BBDD con DataSource*/
+		System.out.println("Vamos a probar a hacer la insercion por DATASOURCE");
+		InitialContext miInitialContext;
+		DataSource miDS;
+		try{
+			miInitialContext = new InitialContext();
+
+			miDS = (DataSource) miInitialContext.lookup("RotolearnJNDI");
+
+			Connection conexion = miDS.getConnection();
+
+			Statement myStatement = conexion.createStatement();
+		
+			//ResultSet rs = myStatement.executeQuery("SELECT * FROM USUARIO WHERE Nickname='"+nick+"'");
+			
+
+		//	if(rs.getString("Nickname") == null){ // si no existe el usuario, puedo crearlo
+				//
+				// Modificar el html para quitar lo de la segunda contraseña y cambiar lo de la imagen por  un textbox
+				//
+				
+			myStatement.executeUpdate("INSERT INTO USUARIO VALUES ('"+request.getParameter("optradio")+"','"+nick+"','"+
+			request.getParameter("nombre")+"','"+request.getParameter("apellido1")+"','"+request.getParameter("apellido2")+
+			"','"+String.valueOf(request.getParameter("pass").hashCode())+"','"+request.getParameter("date")+"','"+request.getParameter("exampleInputFile")
+			+"','"+request.getParameter("email")+"','"+request.getParameter("tlf")+"', '"+
+			request.getParameter("direccion")+"', '"+request.getParameter("descripcion")+"', '"+request.getParameter("intereses")+"')");
+			
+			
+			myStatement.close();
+			conexion.close();
+			
+			//.out.println("El getparameter " +  request.getParameter("nick") + "fin nombre");
+			//System.out.println("voy a meter a este usuario " + newUsuario.getNickname());
+			newUsuario.setNombre(request.getParameter("nombre"));
+			newUsuario.setTipo(request.getParameter("optradio"));
+			newUsuario.setApellido1(request.getParameter("apellido1"));
+			newUsuario.setApellido2(request.getParameter("apellido2"));
+			newUsuario.setEmail(request.getParameter("email"));
+			newUsuario.setPass(String.valueOf(request.getParameter("pass").hashCode()));
+			//String.valueOf(request.getParameter("Password").hashCode());
+			newUsuario.setFecha_nac(request.getParameter("date")); 
+			newUsuario.setDireccion(request.getParameter("direccion"));
+			newUsuario.setDescripcion(request.getParameter("descripcion"));
+			newUsuario.setIntereses(request.getParameter("intereses"));
+			newUsuario.setTelefono(request.getParameter("tlf"));
+			newUsuario.setImagen(request.getParameter("exampleInputFile"));
+		//	}
+			
+	
+			
+		}catch (NamingException e) {
+			// TODO Bloque catch generado automaticamente
+			e.printStackTrace();
+			request.setAttribute("error","reg");
+			ruta = "formulario_registro.jsp";
+
+		} catch (SQLWarning sqlWarning) {
+			while (sqlWarning != null) {
+				System.out.println("Error: " + sqlWarning.getErrorCode());
+				System.out.println("Descripcion: " + sqlWarning.getMessage());
+				System.out.println("SQLstate: " + sqlWarning.getSQLState());
+				sqlWarning = sqlWarning.getNextWarning();
+				request.setAttribute("error","reg");
+				ruta = "formulario_registro.jsp";
+			}
+		} catch (SQLException sqlException) {
+			while (sqlException != null) {
+				System.out.println("Error: " + sqlException.getErrorCode());
+				System.out.println("Descripcion: " + sqlException.getMessage());
+				System.out.println("SQLstate: " + sqlException.getSQLState());
+				sqlException = sqlException.getNextException();
+				request.setAttribute("error","reg");
+				ruta = "formulario_registro.jsp";
+			}
+		}
+
+		
+		
+		
+		
+	/*//////////////////JPA///////////////////////////////////
+		/  LA CONSULTA A LA BBDD
 		// 1 Create the factory of Entity Manager
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("dokulearning");//ESTO ES CLAVE
 
@@ -72,7 +166,7 @@ public class RegistroRequestHandler implements RequestHandler {
 		
 		em.getTransaction().commit();
 		em.close();
-
+	*/
 		return ruta;
 	}
 
